a9eb6cd7c6b3c0e55fdc6e268ea7202aa137e651
vlcp197 <vlcp197@gmail.com>
2024-11-10 18:29:24 -0300

    Add project files.

---
+++ Pong/Globals.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Pong
+{
+    internal class Globals
+    {
+        public static SpriteBatch SpriteBatch;
+        public static int WIDTH = 640;
+        public static int HEIGHT = 480;
+
+
+
+    }
+}
