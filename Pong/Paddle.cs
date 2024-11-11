a551afe4931e43ea53ab85a7d6660635f7a89c5b
vlcp197 <vlcp197@gmail.com>
2024-11-10 21:25:04 -0300

    Build Paddle class to create paddle which the player can move

---
+++ Pong/Paddle.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Pong 
+{
+    public class Paddle
+    {
+        public Rectangle rect;
+        bool isSecondPlayer;
+        float moveSpeed = 500f;
+
+        public Paddle(bool isSecondPlayer) {
+            this.isSecondPlayer = isSecondPlayer;
+            rect = new Rectangle((this.isSecondPlayer ? Globals.WIDTH - 40 : 0), 140, 40, 200);
+        
+        }
+        public void Draw() {
+            Globals.spriteBatch.Draw(Globals.pixel, rect, Color.White);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            KeyboardState kstate = Keyboard.GetState();
+
+            if ((this.isSecondPlayer? kstate.IsKeyDown(Keys.Up) :kstate.IsKeyDown(Keys.W)) && rect.Y > 0)
+            {
+                rect.Y -= (int)(moveSpeed * (float) gameTime.ElapsedGameTime.TotalSeconds);
+            } 
+
+            if ((this.isSecondPlayer?kstate.IsKeyDown(Keys.Down):kstate.IsKeyDown(Keys.S)) && rect.Y < Globals.HEIGHT - rect.Height)
+            {
+                rect.Y += (int)(moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds);
+            }
+
+        }
+    }
+}
